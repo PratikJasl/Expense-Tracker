65ab4367bca14057e67204f0a3058374f20008c2
Pratik Jussal <pratikjaslofficial@gmail.com>
2024-02-25 00:16:46 +0530

    seventh commit

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ export default function LandingPage(){
     return(
         <div className="">
             <img 
-                src="src/components/LandingPage/LandingPagePic.png"
+                src="https://expense-tracker-six-alpha.vercel.app/src/components/LandingPage/LandingPagePic.png"
                 alt="picture"
                 className="h-64 p-4 sm:h-80 md:h-80 lg:h-96"
             />
