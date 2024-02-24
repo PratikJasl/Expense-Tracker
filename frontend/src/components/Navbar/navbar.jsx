e3326356e2dae5e9b54eb8ad72097e0ef847221a
Pratik Jussal <pratikjaslofficial@gmail.com>
2024-02-25 00:27:26 +0530

    tenth commit

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -20,7 +20,7 @@ export default function Navigation(){
     return(
         <Navbar className='bg-white w-full fixed top-0 shadow-sm z-50'>
         <Container>
-            <img src="frontend/src/components/Navbar/logo.jpg" className='h-8 rounded-full'/>
+            <img src="https://expense-tracker-six-alpha.vercel.app/frontend/src/components/Navbar/logo.jpg" className='h-8 rounded-full'/>
             <Navbar.Brand 
                 className="me-auto text-1xl font-bold text-green-600 ml-2"
                 >
